52db6b167c653a35fd29c3d85f7d233600f52168
Yash Kumar Shukla <69341705+yashshukla0918@users.noreply.github.com>
2023-07-14 02:53:45 +0530

    nav bug fix

--- src/Components/NavigationComp.js
+++ src/Components/NavigationComp.js
@@ -6,8 +6,8 @@ const NavigationComp = () => {
                 <span className="navbar-brand mb-0 h1">CIPL Crud App</span>
             </div>
             <div className='container-fluid justify-content-end'>
-                <Link to='/'><button className='btn btn-outline-warning mx-1'>HOME</button></Link>
-                <Link to='/user'><button className='btn btn-outline-warning mx-1'> User Data</button></Link>
+                <Link to='/CIPL_CRUD/'><button className='btn btn-outline-warning mx-1'>HOME</button></Link>
+                <Link to='/CIPL_CRUD/user'><button className='btn btn-outline-warning mx-1'> User Data</button></Link>
             </div>
         </nav>
     )
